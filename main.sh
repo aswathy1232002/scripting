9c3c1430d9cdb9b655c5c7e91279c630049041c5
aswathy <aswathy182002@gmail.com>
2023-07-27 17:51:21 +0530

    third commit

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+alert="\033[1;36m"
+success="\033[1;32m"
+warning="\033[1;33m"
+error="\033[1;31m"
+nocolour="\033[00m"
+
+if [[ -d /home/aswathy/testing ]];then
+echo -e $warning file exist $nocolour
+else 
+mkdir /home/aswathy/testing
+echo -e $success directory created $nocolour
+fi
+if [[ -f home/aswathy/testing/latest.zip ]];then
+echo -e $alert file exist $nocolour
+else 
+wget https://wordpress.org/latest.zip -o /home/aswathy/testing/latest.zip
+echo -e $success file dowloaded $nocolour
+fi
